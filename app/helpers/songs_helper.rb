9e3cf2777233be5fa1ae39a305e58e23c693ec0f
okorkov <alex.okorkov@gmail.com>
2021-01-21 10:21:05 -0800

    commit tree 7b2cbd2c93e1b87d1dc1a86c551c22abbcc4c404

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,9 @@
 module SongsHelper
+  def link_to_artist(song)
+    link_to song.artist.name, artist_path(song.artist)
+  end
 
+  def add_artist(song)
+    link_to "Add Artist", edit_song_path(song)
+  end
 end
